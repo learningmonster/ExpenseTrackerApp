e419a55edb0319d46d9bfe036ebcc8236e6f5fb7
learningmonster <creativemonsterdev@gmail.com>
2021-10-02 15:56:42 -0700

    Splitting components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  // const expenses = [
-  //   {
-  //     title: "Car Insurance",
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   {
-  //     title: "Phone Bill",
-  //     amount: 20.12,
-  //     date: new Date(2021, 1, 14),
-  //   },
-  //   {
-  //     title: "Food",
-  //     amount: 10.52,
-  //     date: new Date(2021, 0, 7),
-  //   },
-  // ];
-
   const expenses = [
     {
       id: "e1",
@@ -49,27 +31,27 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <p>This paragraph is also visible!</p>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,28 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  //   const expenseDate = new Date(2021, 2, 28);
-  //   const expenseTitle = "Car Insurance";
-  //   const expenseAmount = 294.67;
-
-  const expenseDate = props.date;
+  //   const expenseDate = props.date;
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
-  const month = expenseDate.toLocaleString("en-US", { month: "long" });
-  const year = expenseDate.toLocaleString("en-US", { year: "2-digit" });
-  //   const day = expenseDate.toLocaleString("en-US", { month: "numeric" });
-  const day = expenseDate.getFullYear();
+  //   const month = expenseDate.toLocaleString("en-US", { month: "long" });
+  //   const day = expenseDate.toLocaleString("en-US", { day: "2-digit" });
+  //   const year = expenseDate.getFullYear();
 
   return (
     <div className="expense-item">
-      {/* <div>{expenseDate.toISOString()}</div> */}
-      {/* <div>{expenseDate.toDateString()}</div> */}
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
