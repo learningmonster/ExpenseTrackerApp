05be9e02e10cb5858f29f952164cf2b28564cb3a
learningmonster <creativemonsterdev@gmail.com>
2021-10-02 15:21:00 -0700

    Split attribute values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,9 +9,20 @@ function ExpenseItem(props) {
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
+  const month = expenseDate.toLocaleString("en-US", { month: "long" });
+  const year = expenseDate.toLocaleString("en-US", { year: "2-digit" });
+  //   const day = expenseDate.toLocaleString("en-US", { month: "numeric" });
+  const day = expenseDate.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      {/* <div>{expenseDate.toISOString()}</div> */}
+      {/* <div>{expenseDate.toDateString()}</div> */}
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
