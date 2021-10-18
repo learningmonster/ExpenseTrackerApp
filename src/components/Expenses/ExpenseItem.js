572ecc5af988bc98fcc306e5c541fc3f0c542db7
learningmonster <creativemonsterdev@gmail.com>
2021-10-18 13:19:20 -0700

    Using React State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,20 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const expenseTitle = props.title;
+  const [expenseTitle, setExpenseTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
+  // let expenseTitle = props.title;
   const expenseAmount = props.amount;
+
+  const clickHandler = () => {
+    // expenseTitle = "Updated title!!!";
+    setExpenseTitle("Updated title!!!");
+    console.log(expenseTitle);
+  };
 
   return (
     <Card className="expense-item">
@@ -13,6 +23,7 @@ const ExpenseItem = (props) => {
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
